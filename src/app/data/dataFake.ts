954b630a8a837cbc1e46790ffd9f0aeed9d3bafe
Júlia Pereira Galdiano Ribeiro <50510515+JuliaPegari@users.noreply.github.com>
2023-10-17 00:16:04 -0300

    polishing

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Nova atualização!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    imageCover:
+      'https://www.insper.edu.br/wp-content/uploads/2023/04/Linguagen-de-programacao-2-shutterstock_1680857539.jpg.jpeg',
+  },
+  {
+    id: '2',
+    title: 'Novos Recursos chegaram!',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    imageCover: '',
+  },
+];
